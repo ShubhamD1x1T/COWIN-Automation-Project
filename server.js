b8f819bac6997d7a01c763c78df3f562f4bc6840
ShubhamD1x1T <dixitshubham189@gmail.com>
2022-04-01 13:49:33 +0530

    bugs removed

--- server.js
+++ server.js
@@ -49,5 +49,7 @@ app.get("/details/:pin/:age/:email",async function(req,res){
 
 })
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 console.log(":Server is Running......");
